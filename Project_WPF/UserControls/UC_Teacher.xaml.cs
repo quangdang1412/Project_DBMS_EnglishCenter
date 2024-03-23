37e1dec237b9a21be3dff919514b67a979c6d6cb
QuangDang <QuangDang@DESKTOP-588IDI4>
2024-03-23 11:17:34 +0700

    Add project files.

---
+++ Project_WPF/UserControls/UC_Teacher.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_WPF.UserControls
+{
+	/// <summary>
+	/// Interaction logic for UC_Home.xaml
+	/// </summary>
+	public partial class UC_Home : UserControl
+	{
+		public UC_Home()
+		{
+			InitializeComponent();
+		}
+		private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(SearchBox.Text))
+			{
+				Placeholder.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				Placeholder.Visibility = Visibility.Collapsed;
+			}
+		}
+	}
+}
