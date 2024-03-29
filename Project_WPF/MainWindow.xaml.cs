3f20fa19cb1989d194911f53fad64823f271e5ed
QuangDang <QuangDang@DESKTOP-588IDI4>
2024-03-29 22:07:42 +0700

    focus

--- Project_WPF/MainWindow.xaml.cs
+++ Project_WPF/MainWindow.xaml.cs
@@ -31,6 +31,7 @@ namespace Project_WPF
 			this.Left = 0;
 			this.Width = SystemParameters.WorkArea.Width;
 			this.Height = SystemParameters.WorkArea.Height;
+			btn_Home.Focus();
 		}
 
 		private void btn_Home_Click(object sender, RoutedEventArgs e)
@@ -53,5 +54,41 @@ namespace Project_WPF
 			MainGrid.Children.Clear(); // Xóa tất cả các UserControl hiện tại
 			MainGrid.Children.Add(ucStudent);
 		}
+
+		private void btn_Home_GotFocus(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7b5cd6"));
+		}
+
+		private void btn_Home_LostFocus(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			button.Background = Brushes.Transparent;
+		}
+		private void btn_Teacher_GotFocus(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7b5cd6"));
+		}
+
+		private void btn_Teacher_LostFocus(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			button.Background = Brushes.Transparent;
+		}
+
+		private void btn_Student_GotFocus(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7b5cd6"));
+		}
+
+		private void btn_Student_LostFocus(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			button.Background = Brushes.Transparent;
+		}
+
 	}
 }
