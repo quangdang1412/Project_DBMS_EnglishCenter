a8035512f4a799cb0e01824d501381334c552c51
Ngoc Han <papyconheook@gmail.com>
2024-04-09 20:52:28 +0700

    done course

---
+++ BusinessLayer/CourseBLL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class CourseBLL
+    {
+        DAL db = null;
+        public CourseBLL()
+        {
+            db = new DAL();
+        }
+        public DataSet LayCourse()
+        {
+            return db.ExecuteQueryDataSet("select * from COURSE", CommandType.Text, null);
+        }
+        public bool ThemCourse(ref string err, string courseID, string courseName)
+        {
+            // Tạo một mảng chứa các tham số cho thủ tục lưu trữ
+            SqlParameter[] sqlParams =
+            {
+                new SqlParameter("@courseID", courseID),
+                new SqlParameter("@course_name", courseName)     
+            };
+
+            return db.MyExecuteNonQuery("insertCourse", CommandType.StoredProcedure, ref err, sqlParams);
+        }
+        public bool CapNhatCourse(ref string err, string courseID, string courseName)
+        {
+            // Tạo một mảng chứa các tham số cho thủ tục lưu trữ
+            SqlParameter[] sqlParams =
+            {
+                new SqlParameter("@courseID", courseID),
+                new SqlParameter("@course_name", courseName)
+            };
+
+            return db.MyExecuteNonQuery("updateCourse", CommandType.StoredProcedure, ref err, sqlParams);
+        }
+        public bool XoaCourse(ref string err, string courseID)
+        {
+            SqlParameter[] sqlParams =
+            {
+                new SqlParameter("@courseID",courseID),
+            };
+
+            return db.MyExecuteNonQuery("deleteCourse", CommandType.StoredProcedure, ref err, sqlParams);
+        }
+    }
+}
---
+++ Project_WPF/frm_Course.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BusinessLayer;
+
+namespace Project_WPF
+{
+    /// <summary>
+    /// Interaction logic for frm_Course.xaml
+    /// </summary>
+    public partial class frm_Course : Window
+    {
+        CourseBLL dbsCourse;
+        bool check = true;
+        int id = 0;
+        public frm_Course()
+        {
+            InitializeComponent();
+            dbsCourse = new CourseBLL();
+        }
+        public void FillData(DataRowView selectedRow)
+        {
+            // Điền dữ liệu từ hàng được chọn vào các điều khiển trên form         
+            txt_ID.Text = selectedRow["course_ID"].ToString();
+            txt_Ten.Text = selectedRow["course_name"].ToString();             
+            check = false;
+        }
+
+        
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txt_ID.Text) || string.IsNullOrWhiteSpace(txt_Ten.Text) )
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin vào tất cả các trường.");
+                return;
+            }
+            string err = "";                  
+            
+            if (check == true)
+            {
+                try
+                {
+                    bool success = dbsCourse.ThemCourse(ref err, txt_ID.Text, txt_Ten.Text);                       
+
+                    if (success)
+                    {
+                        MessageBox.Show("Đã thêm xong!");
+                        this.Close();
+                    }
+                    
+                }
+                catch (SqlException)
+                {
+                    MessageBox.Show("Không thêm được. Đã xảy ra lỗi!");
+                }
+            }
+            else
+            {
+                bool success = dbsCourse.CapNhatCourse(ref err, txt_ID.Text,txt_Ten.Text);                
+                if (success)
+                {
+                    MessageBox.Show("Đã cập nhật xong!");
+                    this.Close();
+                }
+                
+            }
+        }
+
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
