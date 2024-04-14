c99a5a23f9808209a63399d98600878347709d90
QuangDang <QuangDang@DESKTOP-588IDI4>
2024-04-14 21:34:35 +0700

    clas Chart

--- Project_WPF/UserControls/UC_Class.xaml.cs
+++ Project_WPF/UserControls/UC_Class.xaml.cs
@@ -1,8 +1,12 @@
-﻿using Project_WPF.Form;
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using Project_WPF.Form;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,23 +29,38 @@ namespace Project_WPF.UserControls
         public UC_Class()
         {
             InitializeComponent();
-        }
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "2015",
+                    Values = new ChartValues<double> { 10, 50, 39, 50 }
+                }
+            };
 
+            //adding series will update and animate the chart automatically
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "2016",
+                Values = new ChartValues<double> { 11, 56, 42 }
+            });
+
+            //also adding values updates and animates the chart automatically
+            SeriesCollection[1].Values.Add(48d);
+
+            Labels = new[] { "Maria", "Susan", "Charles", "Frida" };
+            Formatter = value => value.ToString("N");
+
+            DataContext = this;
+        }
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
         private void btn_addclass_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(SearchBox.Text))
-            {
-                Placeholder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                Placeholder.Visibility = Visibility.Collapsed;
-            }
-        }
+       
         private void btn_editdata_Click(object sender, RoutedEventArgs e)
         {
 
