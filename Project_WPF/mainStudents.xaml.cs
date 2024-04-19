eed36922589e0248c09c199deb5d2c71fe0dd27a
QuangDang <QuangDang@DESKTOP-588IDI4>
2024-04-19 10:04:50 +0700

    giao diện teacher và students

---
+++ Project_WPF/mainStudents.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_WPF
+{
+    /// <summary>
+    /// Interaction logic for mainTeachers.xaml
+    /// </summary>
+    public partial class mainStudents : Window
+    {
+        public mainStudents()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Top = 0;
+            this.Left = 0;
+            this.Width = SystemParameters.WorkArea.Width;
+            this.Height = SystemParameters.WorkArea.Height;
+        }
+        private new void GotFocus(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            button.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7b5cd6"));
+        }
+
+        private new void LostFocus(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+        }
+    }
+}
